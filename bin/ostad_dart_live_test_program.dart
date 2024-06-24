38ed05d221d9cb8a6a8be942843beb61a4f313b5
dipondeb <debdipon37@gmail.com>
2024-06-24 23:35:57 +0600

    This is a live test dart program

---
+++ bin/ostad_dart_live_test_program.dart
@@ -0,0 +1,23 @@
+class Media{
+  void play(){
+    print('Playing Media...');
+  }
+}
+class Song extends Media{
+
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play(){
+    print('Playing song by $artist...');
+  }
+}
+void main(){
+  Media media = Media();
+  media.play();
+
+  Song song = Song('Dipon');
+  song.play();
+}
---
+++ test/ostad_dart_live_test_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_dart_live_test_program/ostad_dart_live_test_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
